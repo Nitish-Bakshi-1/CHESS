b81b15d1eccdfc0e8fbe58a270a378a0e79ac87b
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-11 12:37:09 +0530

    fe init

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,5 +1,36 @@
-const ChessBoard = () => {
-  return <div className="text-white w-full">ChessBoard</div>;
+import { Color, PieceSymbol, Square } from "chess.js";
+
+const ChessBoard = ({
+  board,
+}: {
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+}) => {
+  return (
+    <div className="text-white-200">
+      {board.map((row, i) => {
+        return (
+          <div className="flex" key={i}>
+            {row.map((square, j) => {
+              return (
+                <div
+                  key={j}
+                  className={`w-8 h-8 ${
+                    square ? "bg-green-500" : "bg-green-300"
+                  }`}
+                >
+                  {square ? square.type : ""}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ChessBoard;
