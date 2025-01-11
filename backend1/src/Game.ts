53aa82f749c137858009d1237816fdd9200be0ea
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-11 06:03:58 +0530

    init

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,6 +1,6 @@
 import { WebSocket } from "ws";
 import { Chess } from "chess.js";
-import { GAME_OVER } from "./Messages";
+import { GAME_OVER, INIT_GAME, MOVE } from "./Messages";
 
 export class Game {
   public player1: WebSocket;
@@ -13,6 +13,22 @@ export class Game {
     this.player2 = player2;
     this.board = new Chess();
     this.startTime = new Date();
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
   }
   makeMove(socket: WebSocket, move: { from: string; to: string }) {
     if (this.board.moves.length % 2 === 0 && socket != this.player1) {
@@ -24,15 +40,44 @@ export class Game {
     try {
       this.board.move(move);
     } catch (e) {
+      console.log(e);
+
       return;
     }
     if (this.board.isGameOver()) {
       this.player1.emit(
         JSON.stringify({
           type: GAME_OVER,
+          //   This logic suggests that the winner is determined by whose turn it is when the game ends, which might correspond to the opponent winning on the current player's turn.
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      this.player2.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
         })
       );
       return;
+    }
+    if (this.board.moves.length % 2 === 0) {
+      this.player2.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
     }
   }
 }
